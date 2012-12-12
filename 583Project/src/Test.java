ddf58a9bea0628cc411fc8189d6e236294aece0d
Madhu Palani <madster26@gmail.com>
2012-12-12 17:33:44 -0500

    Add base code

---
+++ 583Project/src/Test.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+import com.android.dx.cf.code.BasicBlocker;
+import com.android.dx.dex.file.ClassDefItem;
+import com.android.dx.dex.file.DexFile;
+import com.android.dx.dex.file.EncodedMethod;
+import com.android.dx.rop.*;
+
+public class Test {
+
+	public void test(ClassDefItem inp)
+	{
+		ArrayList<EncodedMethod> methods = inp.getMethods();
+		
+	}
+	
+}
